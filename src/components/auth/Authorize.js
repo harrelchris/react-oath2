1fb11de851b302f3f2cb3dce5eed7e435baa30c7
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-03-04 09:49:37 -0600

    use client storage

--- src/components/auth/Authorize.js
+++ src/components/auth/Authorize.js
@@ -1,19 +1,17 @@
 import React, {useEffect} from "react";
-import EveOnline from "../../oauth2/providers/EveOnline";
 
-function Authorize() {
-  const provider = new EveOnline();
+function Authorize({client, provider}) {
   const [authorizationURL, state] = provider.authorize();
 
   useEffect(() => {
-    localStorage.setItem("state", state);
+    client.storage.setItem("state", state);
     window.location.href = authorizationURL;
   }, []);
 
   return (
     <>
       <h1>Authorize</h1>
-      <p>Redirecting to authorization endpoint</p>
+      <p>Redirecting to authorization endpoint.</p>
     </>
   );
 }
