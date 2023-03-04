bcbcc0a0957ac05af1467f97aeb2741cc96ee65b
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-03-04 09:49:25 -0600

    simplify callback

--- src/components/auth/Callback.js
+++ src/components/auth/Callback.js
@@ -1,25 +1,12 @@
-import React, {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Callback({code, state}) {
+function Callback({client, code, state}) {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (code) {
-      if (state === localStorage.getItem("state")) {
-        localStorage.removeItem("state");
-        localStorage.setItem("code", code);
-        navigate("/auth/login");
-      } else {
-        console.error("Received invalid state. Please try again.");
-        localStorage.removeItem("state");
-        navigate("/");
-      }
-    } else {
-      console.error("Did not receive valid authorization code. Please try again.");
-      localStorage.removeItem("state");
-      navigate("/");
-    }
+    const redirectPath = client.callback(code, state);
+    navigate(redirectPath);
   }, []);
 
   return (
