4da5af9f0611409e4aba4299e2b996f9cbe430ec
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-03-04 09:47:38 -0600

    conditionally render navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Navbar() {
-  return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/auth/authorize">Authorize</Link>
-    </>
-  );
+function Navbar({client}) {
+  if (client.isAuthenticated()) {
+    return (
+      <>
+        <Link to="/">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/auth/logout">Logout</Link>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Link to="/">Home</Link>
+        <Link to="/auth/authorize">Authorize</Link>
+      </>
+    );
+  }
 }
 
 export default Navbar;
