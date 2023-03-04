10a8826c703da905cd7ae6ce9a18905d7cfbe824
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-03-04 05:37:00 -0600

    implement callback component

--- src/components/auth/Callback.js
+++ src/components/auth/Callback.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
-function Callback() {
+function Callback({code, state}) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (code) {
+      if (state === localStorage.getItem("state")) {
+        localStorage.removeItem("state");
+        localStorage.setItem("code", code);
+        navigate("/auth/login");
+      } else {
+        console.error("Received invalid state. Please try again.");
+        localStorage.removeItem("state");
+        navigate("/");
+      }
+    } else {
+      console.error("Did not receive valid authorization code. Please try again.");
+      localStorage.removeItem("state");
+      navigate("/");
+    }
+  }, []);
+
   return (
     <>
       <h1>Callback</h1>
+      <p>Received callback</p>
     </>
   );
 }
