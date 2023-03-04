b3dffdb2f055912ccea5e9d1a523b08850b6e8d2
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-03-04 09:49:16 -0600

    store token values and redirect

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 
-function Login() {
+function Login({client, provider}) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function main() {
+      const code = client.storage.getItem("code");
+      const token = await provider.token(code);
+      const redirectURL = client.login(token);
+      navigate(redirectURL);
+    }
+
+    main().catch((error) => {
+      console.error(error)});
+  }, []);
+
   return (
     <>
       <h1>Login</h1>
